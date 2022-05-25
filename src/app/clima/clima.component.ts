5ceafe1ded7efb6dce0ed89c8886d08a8b019235
Martin-Ulloa <106207292+Martin-Ulloa@users.noreply.github.com>
2022-05-25 02:34:04 -0400

    conversión de fecha y remoción de variables y función sin usar
    se multiplica dt por 1000 porque se espera la variable en milisegundos, se remuevan las variables día y max_dia, se remueva la función para crear un arreglo de los días siguientes que no se usa

--- src/app/clima/clima.component.ts
+++ src/app/clima/clima.component.ts
@@ -10,8 +10,6 @@ import { Observable } from 'rxjs/internal/Observable';
 export class ClimaComponent implements OnInit {
   title = "Información de la comuna"
   climas: Observable<any> | undefined;
-  dia!: Array<Date>;
-  max_dia = 3;
 
   @Input() latitud = ""
 
@@ -26,10 +24,6 @@ export class ClimaComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    for (let i = 0; i <= this.max_dia; i++) {
-      this.dia[i] = new Date();
-      this.dia[i].setDate(this.dia[i].getDate() + i + 1)
-    }
 
   }
 
--- src/app/provincia/provincia.component.ts
+++ src/app/provincia/provincia.component.ts
@@ -4,26 +4,28 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'provincia',
   templateUrl: './provincia.component.html',
-  styleUrls: ['./provincia.component.css'],
+  styleUrls: ['./provincia.component.css']
 })
 export class ProvinciaComponent implements OnInit {
-  title = 'Seleccionar provincia';
+  title = "Seleccionar provinca"
   provincias: any;
   selectedP: any;
-
+  
   @Output() passProvinciaEvent = new EventEmitter<string>();
 
-  @Input() urlParameterP = '';
+  @Input() urlParameterP ="";
 
-  passProvincia(codigoRegion: string) {
+  passProvincia(codigoRegion: string){
     this.passProvinciaEvent.emit(codigoRegion);
   }
 
   constructor(private service: ApiGobiernoService) {}
 
   ngOnChanges(): void {
-    this.provincias = this.service.getProvincia(this.urlParameterP);
+      this.provincias = this.service.getProvincia(this.urlParameterP);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
+
 }
